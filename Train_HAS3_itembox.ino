2cbc8f3f0a4d391b7c41d24096022cac95bba410
DESKTOP-PLMAB5D\이효주 <julie08051@naver.com>
2026-06-09 19:31:27 +0900

    Firmware Update v10

--- Train_HAS3_itembox.ino
+++ Train_HAS3_itembox.ino
@@ -9,7 +9,7 @@
  *
  */
 
-#define FIRMWARE_VER 9
+#define FIRMWARE_VER 10
 #include "Train_HAS3_itembox.h"
 #include "esp_system.h"
 
@@ -17,7 +17,7 @@ void setup()
 {
     Serial.begin(115200);
 
-    has2wifi.Setup("badland_shoot", "Has3");
+    has2wifi.Setup("badland_shoot", "Code3824@");
     TelnetInit();
     Serial.println("MAC: " + WiFi.macAddress());
     ota.setLogStream(Serial);
